102df91e3ee4145f0061d27a12ea825435831783
Andre Heider <a.heider@gmail.com>
2025-06-05 11:26:54 +0200

    allow setting an arbitrary --tag_wahoo_xml file
    Don't just look in the user config directory and the bundled resources/ folder,
    allow passing any filename to --tag_wahoo_xml.
    
    Order of preference:
    - filesystem, either relative or absolute
    - relative to the user config directory
    - relative to the bundled resources directory

--- wahoomc/osm_maps_functions.py
+++ wahoomc/osm_maps_functions.py
@@ -16,8 +16,7 @@ import logging
 
 # import custom python packages
 from wahoomc.file_directory_functions import read_json_file_country_config, create_empty_directories, write_json_file_generic
-from wahoomc.constants_functions import translate_tags_to_keep, \
-    get_tooling_win_path, get_tag_wahoo_xml_path, TagWahooXmlNotFoundError
+from wahoomc.constants_functions import translate_tags_to_keep, get_tooling_win_path
 
 from wahoomc.setup_functions import read_earthexplorer_credentials
 
@@ -528,7 +527,7 @@ class OsmMaps:
 
         log.debug('+ Sorting land* osm files: OK')
 
-    def create_map_files(self, save_cruiser, tag_wahoo_xml, hdd_mode):
+    def create_map_files(self, save_cruiser, tag_conf_file, hdd_mode):
         """
         Creating .map files
         """
@@ -569,14 +568,7 @@ class OsmMaps:
             cmd.append(f'threads={threads}')
             if hdd_mode:
                 cmd.append('type=hd')
-            # add path to tag-wahoo xml file
-            try:
-                cmd.append(
-                    f'tag-conf-file={get_tag_wahoo_xml_path(tag_wahoo_xml)}')
-            except TagWahooXmlNotFoundError:
-                log.error(
-                    'The tag-wahoo xml file was not found: ˚%s˚. Does the file exist and is your input correct?', tag_wahoo_xml)
-                sys.exit()
+            cmd.append(f'tag-conf-file={tag_conf_file}')
 
             run_subprocess_and_log_output(
                 cmd, f'Error in creating map file via Osmosis with tile: {tile["x"]},{tile["y"]}. mapwriter plugin installed?')
